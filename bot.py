4be3f1c1e7a8c6bc938c97f41cdf56ec3535aa9c
puzzlefixyt-crypto <puzzlefixyt@gmail.com>
2026-02-05 20:17:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -104,7 +104,7 @@ threading.Thread(target=run_web, daemon=True).start()
 # ================================
 
 # ================= CONFIG =================
-MIN_GROUP_MEMBERS = 550
+MIN_GROUP_MEMBERS = 555
 
 GROUP_LIMIT_MESSAGE = (
     "🚫 <b>Access Restricted</b>\n\n"
